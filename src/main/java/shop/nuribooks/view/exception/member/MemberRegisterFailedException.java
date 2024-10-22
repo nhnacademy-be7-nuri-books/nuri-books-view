ff7dbfc33197fd5c6c039ea91171c3bd50dad2c9
yeon-so <nordstromm03@gmail.com>
2024-10-22 14:50:38 +0900

    [FEAT] Member 등록 실패 예외 추가

---
+++ src/main/java/shop/nuribooks/view/exception/member/MemberRegisterFailedException.java
@@ -0,0 +1,7 @@
+package shop.nuribooks.view.exception.member;
+
+public class MemberRegisterFailedException extends RuntimeException {
+	public MemberRegisterFailedException(String message) {
+		super(message);
+	}
+}
