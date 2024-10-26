ae22b16732225d590791acb29ef42d87625f0477
yeon-so <nordstromm03@gmail.com>
2024-10-26 21:06:14 +0900

    chore: 사용하지 않는 파일 삭제

--- src/main/java/shop/nuribooks/view/exception/member/MemberRegisterFailedException.java
+++
@@ -1,7 +0,0 @@
-package shop.nuribooks.view.exception.member;
-
-public class MemberRegisterFailedException extends RuntimeException {
-	public MemberRegisterFailedException(String message) {
-		super(message);
-	}
-}
