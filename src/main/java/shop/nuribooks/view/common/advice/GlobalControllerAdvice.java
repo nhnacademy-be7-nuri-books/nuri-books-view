84b9cc971258251dea4d7e737ca4b8856672f1c8
yeon-so <nordstromm03@gmail.com>
2024-10-27 13:50:59 +0900

    fix : 예외를 공통처리 할 수 있도록 수정

--- src/main/java/shop/nuribooks/view/common/advice/GlobalControllerAdvice.java
+++ src/main/java/shop/nuribooks/view/common/advice/GlobalControllerAdvice.java
@@ -1,17 +1,40 @@
 package shop.nuribooks.view.common.advice;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import shop.nuribooks.view.exception.member.MemberRegisterFailedException;
+import lombok.extern.slf4j.Slf4j;
+import shop.nuribooks.view.exception.DefaultServerError;
 
+/**
+ * 공동 예외 처리
+ *
+ *  <p>
+ *      에러 페이지로 넘겨야 할 예외를 처리한다.
+ *  </p>
+ *
+ * @author nuri
+ */
+@Slf4j
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-	@ExceptionHandler(MemberRegisterFailedException.class)
-	public String handleMemberRegisterFailedException(MemberRegisterFailedException ex, Model model) {
-		model.addAttribute("error", ex.getMessage());
-		return "register";
+	/**
+	 * 서버 예외가 발생할 시 처리
+	 *
+	 *  <p>
+	 *  	예외 발생 시 GET /error 로 리다이렉트
+	 *  </p>
+	 *
+	 * @param ex {@link DefaultServerError} 예외
+	 * @param redirectAttributes 예외 메세지 전달하기 위한 속성
+	 * @return 리다이렉트 할 URL (/error)
+	 */
+	@ExceptionHandler(DefaultServerError.class)
+	public String handlerDefaultServerError(DefaultServerError ex, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute(ex.getMessage());
+		return "redirect:/error";
 	}
+
 }
