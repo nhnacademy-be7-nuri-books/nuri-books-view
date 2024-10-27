1c932fb6fd4971896e953cb7b3dbd2847a1ed62c
yeon-so <nordstromm03@gmail.com>
2024-10-27 13:56:26 +0900

    fix : 회원 등록 controller - 예외 수정

--- src/main/java/shop/nuribooks/view/controller/member/MemberController.java
+++ src/main/java/shop/nuribooks/view/controller/member/MemberController.java
@@ -1,62 +1,74 @@
 package shop.nuribooks.view.controller.member;
 
-import java.io.IOException;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import shop.nuribooks.view.dto.member.request.MemberCreateRequest;
-import shop.nuribooks.view.service.member.MemberService;
+import shop.nuribooks.view.dto.member.request.MemberRegisterRequest;
+import shop.nuribooks.view.service.member.MemberServiceImpl;
 
 /**
+ * 회원 관련 Controller
+ *
+ * <p>
+ *     회원 관련 요청을 처리
+ * </p>
  *
  * @author : nuri
- * @d : 1.0
  */
 @Controller()
 @RequiredArgsConstructor
 @Slf4j
 public class MemberController {
 
-	private final MemberService memberService;
+	private final MemberServiceImpl memberService;
 
 	/**
 	 * 회원가입 GET
-	 * @author : nunu
+	 *
 	 * @return register.html
 	 */
 	@GetMapping("/sign-up")
 	public String registerForm() {
-
 		return "register";
 	}
 
 	/**
 	 * 회원가입 POST
-	 * @param userRequest : member API 에서 응답받은 메시지
-	 * @param bindingResult : 검증용
-	 * @return : 검증 성공 및 memberService 에서 2xx code 받을 시 redirect:login
-	 * 			 예외 발생 시 register.html
+	 *
+	 * <p>
+	 * 회원 등록 정보를 받아 검증 후, 성공 시 로그인 페이지로 리다이렉트
+	 * 실패 시 입력된 정보를 다시 회원가입 페이지로 리다이렉트 후 오류 메시지를 표시
+	 * </p>
+	 *
+	 * @param redirectAttributes 리다이렉트 시 전달할 속성
+	 * @param userRequest 검증을 위한 {@link MemberRegisterRequest} 객체로
+	 *                    사용자가 입력한 회원 등록 정보를 포함
+	 * @return 회원가입 성공 시 로그인 페이지로 리다이렉트하는 URL,
+	 *         실패 시 회원가입 페이지로 리다이렉트하는 URL
 	 */
 	@PostMapping("/sign-up")
-	public void registerUser(
-		HttpServletResponse response,
-		@Valid @ModelAttribute MemberCreateRequest userRequest,
-		BindingResult bindingResult
-	) throws IOException {
-		if (bindingResult.hasErrors()) {
-			// todo : 화면단에 검증 에러를 띄우기 위해 addAttribute 로 입력한 데이터 가져오기..
-			response.sendRedirect("redirect:/register");
-		}
+	public String registerUser(
+		RedirectAttributes redirectAttributes,
+		@Valid @ModelAttribute MemberRegisterRequest userRequest
+	) {
 
-		memberService.registerUser(userRequest);
-		response.sendRedirect("redirect:/login");
+		String returnMessage = memberService.registerUser(userRequest);
+		ModelAndView modelAndView = new ModelAndView();
+
+		if (returnMessage.startsWith("Success")) {
+			return "redirect:/login";
+		} else {
+			redirectAttributes.addFlashAttribute("userRequest", userRequest);
+			redirectAttributes.addFlashAttribute("errorMessage", returnMessage);
+			return "redirect:/sign-up";
+		}
 	}
+
 }
