8795b93c5ce5085049daa705cc4e2f444c4bbac9
yeon-so <nordstromm03@gmail.com>
2024-10-27 14:05:14 +0900

    fix : member 등록 관련 dto 수정

--- src/main/java/shop/nuribooks/view/dto/member/request/MemberRegisterRequest.java
+++ src/main/java/shop/nuribooks/view/dto/member/request/MemberRegisterRequest.java
@@ -2,38 +2,53 @@ package shop.nuribooks.view.dto.member.request;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
-public class MemberCreateRequest {
-
+/**
+ * 회원 등록 요청 데이터 전송 객체.
+ *
+ * <p>
+ * 회원가입 시 클라이언트에서 입력한 정보를 포함,
+ * 입력값의 유효성을 검사
+ * </p>
+ *
+ * @param name 회원의 이름
+ * @param userId 회원의 사용자 ID
+ * @param password 회원의 비밀번호
+ * @param phoneNumber 회원의 휴대폰 번호
+ * @param email 회원의 이메일 주소
+ * @param birthday 회원의 생일 (yyyy-MM-dd 형식)
+ */
+public record MemberRegisterRequest(
 	@NotBlank(message = "사용자 이름은 필수 입력입니다.")
 	@Size(min = 2, max = 30, message = "사용자 이름은 2자 이상 30자 이하로 입력해야 합니다.")
-	private String name;
+	String name,
 
-	@NotBlank(message = "사용자 아이디은 필수 입력입니다.")
-	@Size(min = 4, max = 20, message = "사용자 아이디는 4자 이상 20자 이하로 입력해야 합니다.")
-	private String userId;
+	@NotBlank(message = "사용자 아이디는 필수 입력입니다.")
+	@Size(min = 8, max = 20, message = "사용자 아이디는 8자 이상 20자 이하로 입력해야 합니다.")
+	String userId,
 
-	@NotBlank(message = "사용자 비밀번호은 필수 입력입니다.")
+	@NotBlank(message = "사용자 비밀번호는 필수 입력입니다.")
 	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해야 합니다.")
-	private String password;
+	String password,
 
 	@NotBlank(message = "휴대폰 번호는 필수 입력입니다.")
 	@Pattern(regexp = "^\\d{10,13}$", message = "휴대폰 번호는 10자에서 13자 사이의 숫자로 입력해야 합니다.")
-	private String phoneNumber;
+	String phoneNumber,
 
 	@NotBlank(message = "이메일은 필수 입력입니다.")
 	@Email(message = "유효한 이메일 형식이 아닙니다.")
 	@Size(min = 5, max = 50, message = "이메일은 5자 이상 50자 이하로 입력해야 합니다.")
-	private String email;
+	String email,
 
 	@NotNull(message = "생일은 반드시 입력해야 합니다.")
-	private LocalDate birthday;
-
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+	LocalDate birthday
+) {
 }
