2ee07072014749b3e8dba3952b519100925f2833
yeon-so <nordstromm03@gmail.com>
2024-10-27 14:13:31 +0900

    feat : 홈, 에러 controller 추가

---
+++ src/main/java/shop/nuribooks/view/controller/ErrorController.java
@@ -0,0 +1,18 @@
+package shop.nuribooks.view.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 에러 처리 Controller
+ *
+ * @author nuri
+ */
+@Controller
+public class ErrorController {
+
+	@GetMapping("/error")
+	public String error() {
+		return "error";
+	}
+}
