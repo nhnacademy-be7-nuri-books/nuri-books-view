200849bd07210f64854d9d4e9b495159c6dc76d5
yeon-so <nordstromm03@gmail.com>
2024-10-27 21:40:52 +0900

    Revert "chore : 객체 이름 수정"
    This reverts commit af29c2b791040a49f247390b9851bfe9eb960236.

--- src/main/java/shop/nuribooks/view/controller/member/MemberController.java
+++ src/main/java/shop/nuribooks/view/controller/member/MemberController.java
@@ -12,7 +12,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.nuribooks.view.dto.member.request.MemberRegisterRequest;
-import shop.nuribooks.view.service.member.MemberService;
+import shop.nuribooks.view.service.member.MemberServiceImpl;
 
 /**
  * 회원 관련 Controller
@@ -28,7 +28,7 @@ import shop.nuribooks.view.service.member.MemberService;
 @Slf4j
 public class MemberController {
 
-	private final MemberService memberService;
+	private final MemberServiceImpl memberService;
 
 	@Value("${error.message-key}")
 	private String errorMessageKey;
