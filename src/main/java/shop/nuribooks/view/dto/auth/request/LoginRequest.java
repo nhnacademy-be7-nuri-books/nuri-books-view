8863388825d9188111350c686bd21d31a08f0d40
yeon-so <nordstromm03@gmail.com>
2024-10-27 15:18:23 +0900

    feat : 로그인 dto 추가

---
+++ src/main/java/shop/nuribooks/view/dto/auth/request/LoginRequest.java
@@ -0,0 +1,22 @@
+package shop.nuribooks.view.dto.auth.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * 로드인 요청 데이터 객체
+ *
+ * @param username 사용자 ID
+ * @param password 비밀번호
+ */
+public record LoginRequest(
+	// todo : 왜 회원 등록할 떄는 userId 인데 로그인할때는 username 인지???
+	@NotBlank(message = "사용자 이름은 필수 입력입니다.")
+	@Size(min = 8, max = 20, message = "사용자 아이디는 8자 이상 20자 이하로 입력해야 합니다.")
+	String username,
+
+	@NotBlank(message = "비밀번호는 필수 입력입니다.")
+	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해야 합니다.")
+	String password
+) {
+}
