e8c455986eabb1c6f68dbf0b1150e42b7a6d6bf4
yeon-so <nordstromm03@gmail.com>
2024-10-29 15:03:08 +0900

    feat : Interceptor 설정을 위한 WebConfig 추가

---
+++ src/main/java/shop/nuribooks/view/common/config/WebConfig.java
@@ -0,0 +1,40 @@
+package shop.nuribooks.view.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import shop.nuribooks.view.common.interceptor.LoginCheckInterceptor;
+
+/**
+ * Spring Web 를 구성하는 설정 클래스
+ *
+ * @author nuri
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private final LoginCheckInterceptor loginCheckInterceptor;
+
+	/**
+	 * 생성자
+	 *
+	 * @param loginCheckInterceptor 로그인 체크 인터셉터
+	 */
+	public WebConfig(LoginCheckInterceptor loginCheckInterceptor) {
+		this.loginCheckInterceptor = loginCheckInterceptor;
+	}
+
+	/**
+	 * 로그인 체크 인터셉터
+	 *
+	 * @param registry registry 인터셉터를 등록하는 데 사용되는 {@link InterceptorRegistry} 객체
+	 */
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loginCheckInterceptor)
+			.excludePathPatterns("/eureka/**", "/static/**", "/error")
+			.addPathPatterns("/**");
+	}
+
+}
