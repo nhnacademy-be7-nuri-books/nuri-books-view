db27708346dad2378646471bdc1545c067cd5935
ljg0610 <jggy0627@naver.com>
2024-11-04 19:14:00 +0900

    feat: 알라딘 도서 조회 서비스 interface 추가

---
+++ src/main/java/shop/nuribooks/view/book/service/AladinBookService.java
@@ -0,0 +1,9 @@
+package shop.nuribooks.view.book.service;
+
+import java.util.List;
+
+import shop.nuribooks.view.book.dto.AladinBookListItemResponse;
+
+public interface AladinBookService {
+	List<AladinBookListItemResponse> getAladinBookList();
+}
