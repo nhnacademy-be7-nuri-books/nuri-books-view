98d88398ebf1544fa542ff439287fa3ee52e77d5
jaaaayk <janghyun30@gmail.com>
2024-11-04 18:00:03 +0900

    feature: 관리자 메인페이지 추가

---
+++ src/main/java/shop/nuribooks/view/admin/controller/AdminController.java
@@ -0,0 +1,25 @@
+package shop.nuribooks.view.admin.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+@Controller
+public class AdminController {
+
+	/**
+	 * admin GET
+	 *
+	 * @return admin.html
+	 */
+	@Operation(summary = "로그인 폼", description = "로그인 페이지를 반환합니다.")
+	@GetMapping("/admin")
+	public String adminHome() throws IOException {
+		return "admin/admin";
+	}
+
+}
