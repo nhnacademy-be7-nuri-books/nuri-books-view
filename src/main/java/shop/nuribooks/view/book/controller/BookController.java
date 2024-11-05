d3391fc36ca18709dee1485f4f2bd7a34b129b25
ljg0610 <jggy0627@naver.com>
2024-11-05 21:16:58 +0900

    feat: 도서목록조회 api 추가

---
+++ src/main/java/shop/nuribooks/view/book/controller/BookController.java
@@ -0,0 +1,26 @@
+package shop.nuribooks.view.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.RequiredArgsConstructor;
+import shop.nuribooks.view.book.dto.AdminBookListResponse;
+import shop.nuribooks.view.book.service.BookService;
+import shop.nuribooks.view.common.dto.PagedResponse;
+
+@RequiredArgsConstructor
+@Controller
+public class BookController {
+	private final BookService bookService;
+
+	@GetMapping("/api/view/books")
+	public String getBooks(@RequestParam(defaultValue = "0") int page,
+							@RequestParam(defaultValue = "10") int size,
+							Model model) {
+		PagedResponse<AdminBookListResponse> books = bookService.getBooks(page, size);
+		model.addAttribute("books", books);
+		return "book/bookList";
+	}
+}
