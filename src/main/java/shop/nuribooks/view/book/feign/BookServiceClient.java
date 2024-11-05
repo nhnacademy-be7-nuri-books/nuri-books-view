558aaf435c0bc57f75c6e1005db9918498ce9182
ljg0610 <jggy0627@naver.com>
2024-11-05 21:08:21 +0900

    feat: feignclient 도서목록 조회 메서드 추가

--- src/main/java/shop/nuribooks/view/book/feign/BookServiceClient.java
+++ src/main/java/shop/nuribooks/view/book/feign/BookServiceClient.java
@@ -2,7 +2,13 @@ package shop.nuribooks.view.book.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import shop.nuribooks.view.book.dto.AdminBookListResponse;
+import shop.nuribooks.view.common.dto.PagedResponse;
 
 @FeignClient(name = "book", url = "http://localhost:8083")
 public interface BookServiceClient {
+	@GetMapping("/api/books")
+	PagedResponse<AdminBookListResponse> getBooks(@RequestParam("page") int page, @RequestParam("size") int size);
 }
