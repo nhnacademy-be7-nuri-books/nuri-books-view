98964ad7f6bf2497919fea5fb21ced3ec9e693bd
ljg0610 <jggy0627@naver.com>
2024-11-06 00:29:58 +0900

    fix: 도서목록 조회 설정 위한 requestparam 추가

--- src/main/java/shop/nuribooks/view/book/service/AladinBookServiceImpl.java
+++ src/main/java/shop/nuribooks/view/book/service/AladinBookServiceImpl.java
@@ -3,6 +3,7 @@ package shop.nuribooks.view.book.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import shop.nuribooks.view.book.dto.AladinBookListItemResponse;
@@ -15,8 +16,9 @@ public class AladinBookServiceImpl implements AladinBookService{
 	private final AladinBookServiceClient aladinBookServiceClient;
 
 	@Override
-	public List<AladinBookListItemResponse> getAladinBookList() {
-		List<AladinBookListItemResponse> response = aladinBookServiceClient.getAladinBookList().getBody();
+	public List<AladinBookListItemResponse> getAladinBookList(String queryType, String searchTarget, int maxResults) {
+		List<AladinBookListItemResponse> response =
+			aladinBookServiceClient.getAladinBookList(queryType, searchTarget, maxResults).getBody();
 		if (response != null) {
 			return response;
 		}
