46969085a411df754ac3225e5d5eb240bc908e74
Yongtaek Lee <itaekit@gmail.com>
2024-11-06 17:03:40 +0900

    feat: Payco Resource Server로 부터 인증 유저 정보 받아오기
    - Access Token 기반으로 Resource Server에 유저 정보 요청 및 받아오기 구현
    - PaycoUser는 개발자 센터 API에 따라 필요 정보 추출(idNo, email)

---
+++ src/main/java/shop/nuribooks/view/oauth/dto/PaycoUser.java
@@ -0,0 +1,4 @@
+package shop.nuribooks.view.oauth.dto;
+
+public record PaycoUser(String idNo, String email) {
+}
--- src/main/java/shop/nuribooks/view/oauth/service/OAuth2Service.java
+++ src/main/java/shop/nuribooks/view/oauth/service/OAuth2Service.java
@@ -1,13 +1,16 @@
 package shop.nuribooks.view.oauth.service;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.nuribooks.view.oauth.common.feign.PaycoTokenFeignClient;
+import shop.nuribooks.view.oauth.common.feign.PaycoUserInfoFeignClient;
 import shop.nuribooks.view.oauth.common.property.OAuth2ClientProperties;
+import shop.nuribooks.view.oauth.dto.PaycoUser;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -15,6 +18,7 @@ import shop.nuribooks.view.oauth.common.property.OAuth2ClientProperties;
 public class OAuth2Service {
 	private final OAuth2ClientProperties oAuth2ClientProperties;
 	private final PaycoTokenFeignClient paycoTokenFeignClient;
+	private final PaycoUserInfoFeignClient paycoUserInfoFeignClient;
 
 	public String getPaycoLoginFormUri() {
 		return oAuth2ClientProperties.getProvider().getPayco().getAuthorizationUri()
@@ -23,7 +27,7 @@ public class OAuth2Service {
 			+ "&redirect_uri=" + oAuth2ClientProperties.getRegistration().getPayco().getRedirectUri();
 	}
 
-	public String paycoLogin(String code) {
+	public Optional<PaycoUser> paycoLogin(String code) {
 		Map<String, Object> tokenResponse = paycoTokenFeignClient.getToken(
 			oAuth2ClientProperties.getRegistration().getPayco().getClientId(),
 			oAuth2ClientProperties.getRegistration().getPayco().getClientSecret(),
@@ -32,12 +36,28 @@ public class OAuth2Service {
 		);
 
 		String accessToken = getAccessToken(tokenResponse);
-		
-		return "ok";
+		Map<String, Object> userResponse = paycoUserInfoFeignClient.getUserRequest(
+			oAuth2ClientProperties.getRegistration().getPayco().getClientId(),
+			accessToken
+		);
+
+		Optional<PaycoUser> paycoUser = null;
+		if (isSuccessful(userResponse)) {
+			paycoUser = Optional.of(getPaycoUser(userResponse));
+		}
+		return paycoUser;
 	}
 
-	public String getAccessToken(Map<String, Object> tokenResponse) {
+	private String getAccessToken(Map<String, Object> tokenResponse) {
 		return tokenResponse.get("access_token").toString();
 	}
 
+	private PaycoUser getPaycoUser(Map<String, Object> userResponse) {
+		Map<String, Object> member = (Map<String, Object>) ((Map<String, Object>)userResponse.get("data")).get("member");
+		return new PaycoUser(member.get("idNo").toString(), member.get("email").toString());
+	}
+
+	private boolean isSuccessful(Map<String, Object> userResponse) {
+		return ((Map<String, Object>)userResponse.get("header")).get("isSuccessful").toString().equalsIgnoreCase("true");
+	}
 }
