b9fc8ca967be018e6f62ac47ab0bed28eb6159ed
yeon-so <nordstromm03@gmail.com>
2024-11-07 23:01:02 +0900

    feat: 토큰 재발행 필터 추가

---
+++ src/main/java/shop/nuribooks/view/common/feign/ReissueServiceClient.java
@@ -0,0 +1,19 @@
+package shop.nuribooks.view.common.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+/**
+ * auth 관련 FeignClient
+ * name : Eureka 에 등록된 서비스 이름
+ * contextId : FeignClient 를 구분하기 위한 이름
+ *
+ * @author nuri
+ */
+@FeignClient(name = "reissue", url = "http://localhost:8080")
+public interface ReissueServiceClient {
+	
+	@PostMapping("/api/auth/reissue")
+	ResponseEntity<String> reissue();
+}
