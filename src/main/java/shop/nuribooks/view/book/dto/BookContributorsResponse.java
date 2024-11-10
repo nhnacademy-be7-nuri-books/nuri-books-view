e70b6619cb1dad0cf133801eeaa4f27737ba78b9
ljg0610 <jggy0627@naver.com>
2024-11-10 13:57:37 +0900

    feat: toast ui 적용

--- src/main/java/shop/nuribooks/view/book/dto/BookContributorsResponse.java
+++ src/main/java/shop/nuribooks/view/book/dto/BookContributorsResponse.java
@@ -3,8 +3,6 @@ package shop.nuribooks.view.book.dto;
 import java.util.List;
 import java.util.Map;
 
-import shop.nuribooks.view.bookcontributor.dto.BookContributorInfoResponse;
-
 public record BookContributorsResponse(
 	AdminBookListResponse bookDetails,
 	Map<String, List<String>> contributorsByRole
