d7840e0dbed2f27e7b4b3ccfe8a1d4ad36285072
Yongtaek Lee <itaekit@gmail.com>
2024-11-11 10:16:01 +0900

    fix: AdminCheckFilter 활성화
    - ADMIN 권한에 대해서 통과 시키는 필터 활성화

--- src/main/java/shop/nuribooks/view/common/filter/AdminCheckFilter.java
+++ src/main/java/shop/nuribooks/view/common/filter/AdminCheckFilter.java
@@ -1,7 +1,9 @@
 package shop.nuribooks.view.common.filter;
 
 import java.io.IOException;
+import java.util.Objects;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -9,6 +11,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import shop.nuribooks.view.common.decoder.JwtDecoder;
+import shop.nuribooks.view.common.util.CookieUtil;
+import shop.nuribooks.view.exception.UnauthorizedException;
 
 /**
  * 어드민 권한 확인 필터
@@ -21,22 +26,21 @@ public class AdminCheckFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
-		// todo : admin 페이지 권한 차단 로직 - 배포시 활성화
-		// if (!request.getRequestURI().matches("^/admin(/.*)?$")) {
-		// 	filterChain.doFilter(request, response);
-		// 	return;
-		// }
-		//
-		// String accessToken = CookieUtil.findByCookieKey(request, HttpHeaders.AUTHORIZATION);
-		//
-		// if (Objects.isNull(accessToken)) {
-		// 	response.sendRedirect("/login");
-		// 	return;
-		// }
-		//
-		// if (!JwtDecoder.getRole(accessToken).contains("ADMIN")) {
-		// 	throw new UnauthorizedException();
-		// }
+		if (!request.getRequestURI().matches("^/admin(/.*)?$")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String accessToken = CookieUtil.findByCookieKey(request, HttpHeaders.AUTHORIZATION);
+
+		if (Objects.isNull(accessToken)) {
+			response.sendRedirect("/login");
+			return;
+		}
+
+		if (!JwtDecoder.getRole(accessToken).contains("ADMIN")) {
+			throw new UnauthorizedException();
+		}
 
 		filterChain.doFilter(request, response);
 	}
