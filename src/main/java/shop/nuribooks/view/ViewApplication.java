35663ce5fad1500442f825b1dcaacb5664c610dc
Yongtaek Lee <itaekit@gmail.com>
2024-11-12 15:52:56 +0900

    feat: redis session 추가

--- src/main/java/shop/nuribooks/view/ViewApplication.java
+++ src/main/java/shop/nuribooks/view/ViewApplication.java
@@ -3,9 +3,11 @@ package shop.nuribooks.view;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableRedisHttpSession
 public class ViewApplication {
 
 	public static void main(String[] args) {
