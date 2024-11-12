d90da93a97ef1af7a61d1dde69f88a8be49b8f42
kkyongg <kkyongg2@gmail.com>
2024-11-12 15:44:30 +0900

    feat: 태그 관리자 페이지 수정

--- src/main/java/shop/nuribooks/view/admin/tag/controller/TagController.java
+++ src/main/java/shop/nuribooks/view/admin/tag/controller/TagController.java
@@ -1,21 +1,16 @@
 package shop.nuribooks.view.admin.tag.controller;
 
-import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
 import shop.nuribooks.view.admin.tag.dto.TagRequest;
 import shop.nuribooks.view.admin.tag.dto.TagResponse;
 import shop.nuribooks.view.admin.tag.service.TagService;
-import shop.nuribooks.view.exception.ResourceAlreadyExistsException;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @Slf4j
@@ -25,48 +20,40 @@ public class TagController {
 
     private final TagService tagService;
 
-    @Value("${error.message-key}")
-    private String errorMessageKey;
-
-    @Value("${success.message-key}")
-    private String successMessageKey;
-
     @GetMapping
-    public String showRegisterTagForm(Model model) {
+    public String showTagList(Model model) {
         List<TagResponse> tags = tagService.getAllTags();
         model.addAttribute("tags", tags);
-        return "admin/tag";
+        return "admin/tag/tag-list";
+    }
+
+    @GetMapping("/register")
+    public String showTagRegisterPage() {
+        return "admin/tag/tag-register";
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, String>> registerTag(TagRequest request) {
-        try {
-            tagService.registerTag(request);
-            return ResponseEntity.ok(Map.of(request.name(), "등록 성공"));
-        }catch (ResourceAlreadyExistsException ex) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(Map.of(HttpStatus.CONFLICT.toString(), ex.getMessage()));
-        }
+    public String registerTag(TagRequest tagRequest) {
+        tagService.registerTag(tagRequest);
+        return "redirect:/admin/tag";
     }
 
-    @PostMapping("/edit/{id}")
-    public ResponseEntity<Map<String, String>> updateTag(@PathVariable Long id, @RequestBody TagRequest tagRequest) {
-        try {
-            tagService.updateTag(id, tagRequest);
-            return ResponseEntity.ok(Map.of(tagRequest.name(), "수정 성공"));
-        }catch (ResourceAlreadyExistsException ex) {
-            return ResponseEntity.status(HttpStatus.CONFLICT)
-                    .body(Map.of("status", "error", "message", ex.getMessage()));
-        }
+    @GetMapping("/{tag-id}")
+    public String showEditTagPage(@PathVariable("tag-id") Long id, Model model) {
+        TagResponse tag = tagService.getTag(id);
+        model.addAttribute("tag", tag);
+        return "admin/tag/tag-edit";
     }
 
-    @PostMapping("/delete/{id}")
-    public ResponseEntity<Map<String, String>> deleteTag(@PathVariable Long id) {
-        try {
-            tagService.deleteTag(id);
-            return ResponseEntity.ok(Map.of("data", "삭제 성공"));
-        } catch (FeignException ex) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("status", "error", "message", ex.getMessage()));
-        }
+    @PutMapping("/{tag-id}")
+    public String updateTag(@PathVariable("tag-id") Long id, TagRequest tagRequest) {
+        tagService.updateTag(id, tagRequest);
+        return "redirect:/admin/tag";
+    }
+
+    @DeleteMapping("/{tag-id}")
+    public String deleteTag(@PathVariable("tag-id") Long id) {
+        tagService.deleteTag(id);
+        return "redirect:/admin/tag";
     }
 }
