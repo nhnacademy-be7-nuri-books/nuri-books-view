bdc8e3fea87dc66fb385af578658fda184278b9f
Yongtaek Lee <143694269+itaekit@users.noreply.github.com>
2024-11-13 10:31:58 +0900

    Revert "Feature/cart 장바구니 뷰 추가 (#70)"
    This reverts commit 9443c51ba78b93df00a66b160305832c6d7e9ee2.

--- src/main/java/shop/nuribooks/view/cart/dto/request/CartAddRequestToServer.java
+++
@@ -1,7 +0,0 @@
-package shop.nuribooks.view.cart.dto.request;
-
-public record CartAddRequestToServer(
-	String cartId,
-	Long bookId,
-	int quantity) {
-}
--- src/main/java/shop/nuribooks/view/cart/dto/response/CartBookResponse.java
+++
@@ -1,9 +0,0 @@
-package shop.nuribooks.view.cart.dto.response;
-
-import java.math.BigDecimal;
-
-public record CartBookResponse(String title,
-							   BigDecimal price,
-							   int discountRate,
-							   String thumbnailImageUrl) {
-}
