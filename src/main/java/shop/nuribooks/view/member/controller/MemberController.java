abb58c92a629fad989efdba2d66b274be6e09d8a
Jprotection <bigho98@gmail.com>
2024-11-13 13:54:01 +0900

    refactor: 수정

--- src/main/java/shop/nuribooks/view/member/controller/MemberController.java
+++ src/main/java/shop/nuribooks/view/member/controller/MemberController.java
@@ -2,13 +2,11 @@ package shop.nuribooks.view.member.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import io.swagger.v3.oas.annotations.Operation;
