a3835ae6dc56592e15d8ca321dad786db49ffe05
kyongmin <110990435+kkyongg@users.noreply.github.com>
2024-11-20 16:18:27 +0900

    Feature/message (#108)
    * feat: publisher 완료메세지 추가
    
    * feat: tag 완료메세지 추가

--- src/main/java/shop/nuribooks/view/admin/publisher/service/PublisherServiceImpl.java
+++ src/main/java/shop/nuribooks/view/admin/publisher/service/PublisherServiceImpl.java
@@ -1,95 +1,47 @@
 package shop.nuribooks.view.admin.publisher.service;
 
-import feign.FeignException;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import shop.nuribooks.view.admin.contributor.dto.ContributorResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import shop.nuribooks.view.admin.publisher.PublisherServiceClient;
 import shop.nuribooks.view.admin.publisher.dto.PublisherRequest;
 import shop.nuribooks.view.admin.publisher.dto.PublisherResponse;
-import shop.nuribooks.view.common.util.ExceptionUtil;
-import shop.nuribooks.view.exception.ResourceAlreadyExistsException;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-
-import javax.naming.ServiceUnavailableException;
+import shop.nuribooks.view.common.dto.ResponseMessage;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class PublisherServiceImpl implements PublisherService {
 
-    private final PublisherServiceClient publisherServiceClient;
-    @Value("${success.message-key}")
-    private String successMessageKey;
+	private final PublisherServiceClient publisherServiceClient;
 
-    @Override
-    public void registerPublisher(PublisherRequest publisherRequest) {
-        try {
-            PublisherResponse response = publisherServiceClient.registerPublisher(publisherRequest).getBody();
-            Objects.requireNonNull(response);
-        } catch (FeignException ex) {
-            if (ex.status() == HttpStatus.CONFLICT.value()) {
-                throw new ResourceAlreadyExistsException(ex.getMessage());
-            }
-            ExceptionUtil.handleFeignException(ex);
-        }
-    }
+	@Override
+	public ResponseMessage registerPublisher(PublisherRequest publisherRequest) {
+		return publisherServiceClient.registerPublisher(publisherRequest).getBody();
 
-    // @Override
-    // public List<PublisherResponse> getAllPublishers() {
-    //     try {
-    //         return publisherServiceClient.getAllPublishers().getBody();
-    //     } catch (FeignException ex) {
-    //         return Collections.emptyList();
-    //     }
-    // }
-    @Override
-    public Page<PublisherResponse> getAllPublishers(Pageable pageable) {
-        try {
-            // Feign 호출
-            return publisherServiceClient.getAllPublishers(pageable.getPageNumber(), pageable.getPageSize()).getBody();
-        } catch (FeignException ex) {
-            throw new RuntimeException("Unknown error while fetching publishers", ex);
-        }
-    }
+	}
 
-    @Override
-    public PublisherResponse getPublisher(Long publisherId) {
-        try {
-            return publisherServiceClient.getPublisherById(publisherId).getBody();
-        } catch (FeignException ex) {
-            String errorMessage = ExceptionUtil.handleFeignException(ex);
-            return PublisherResponse.error(errorMessage);
-        }
-    }
+	@Override
+	public Page<PublisherResponse> getAllPublishers(Pageable pageable) {
+		return publisherServiceClient.getAllPublishers(pageable.getPageNumber(), pageable.getPageSize()).getBody();
 
-    public void updatePublisher(Long id, PublisherRequest publisherRequest) {
-        try {
-            PublisherResponse response = publisherServiceClient.updatePublisher(id, publisherRequest).getBody();
-            Objects.requireNonNull(response);
-        } catch (FeignException ex) {
-            if (ex.status() == HttpStatus.CONFLICT.value()) {
-                throw new ResourceAlreadyExistsException(ex.getMessage());
-            }
-            ExceptionUtil.handleFeignException(ex);
-        }
-    }
+	}
 
-    @Override
-    public void deletePublisher(Long id) {
-        try {
-            publisherServiceClient.deletePublisher(id);
-        } catch (FeignException ex) {
-            ExceptionUtil.handleFeignException(ex);
-        }
-    }
+	@Override
+	public PublisherResponse getPublisher(Long publisherId) {
+		return publisherServiceClient.getPublisherById(publisherId).getBody();
+	}
+
+	public ResponseMessage updatePublisher(Long id, PublisherRequest publisherRequest) {
+		return publisherServiceClient.updatePublisher(id, publisherRequest).getBody();
+	}
+
+	@Override
+	public ResponseMessage deletePublisher(Long id) {
+		return publisherServiceClient.deletePublisher(id).getBody();
+
+	}
 }
--- src/main/java/shop/nuribooks/view/admin/tag/service/TagService.java
+++ src/main/java/shop/nuribooks/view/admin/tag/service/TagService.java
@@ -1,23 +1,25 @@
 package shop.nuribooks.view.admin.tag.service;
 
-import jakarta.validation.Valid;
-import shop.nuribooks.view.admin.tag.dto.TagRequest;
-import shop.nuribooks.view.admin.tag.dto.TagResponse;
-
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import jakarta.validation.Valid;
+import shop.nuribooks.view.admin.tag.dto.TagRequest;
+import shop.nuribooks.view.admin.tag.dto.TagResponse;
+import shop.nuribooks.view.common.dto.ResponseMessage;
+
 public interface TagService {
-    void registerTag(@Valid TagRequest tagRequest);
-    List<TagResponse> getAllTags();
+	ResponseMessage registerTag(@Valid TagRequest tagRequest);
 
-    Page<TagResponse> getAllTags(Pageable pageable);
+	List<TagResponse> getAllTags();
 
-    TagResponse getTag(Long id);
+	Page<TagResponse> getAllTags(Pageable pageable);
 
-    void updateTag(@Valid Long id, TagRequest request);
+	TagResponse getTag(Long id);
 
-    void deleteTag(@Valid Long id);
+	ResponseMessage updateTag(@Valid Long id, TagRequest request);
+
+	ResponseMessage deleteTag(@Valid Long id);
 }
