dc2664ba38d5b2b93a989397d1483710e6221666
Geonu Lim <44187187+rjsdn0124@users.noreply.github.com>
2024-11-20 16:17:00 +0900

    Feature/elastic search (#109)
    * feat: search view
    
    * feat: search list view.
    
    * feat: 정렬 완성.

---
+++ src/main/java/shop/nuribooks/view/booksearch/feign/BookSearchServiceClient.java
@@ -0,0 +1,23 @@
+package shop.nuribooks.view.booksearch.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import shop.nuribooks.view.booksearch.dto.BookSearchResponse;
+import shop.nuribooks.view.booksearch.enums.SearchType;
+import shop.nuribooks.view.booksearch.enums.SortType;
+
+@FeignClient(name = "search", url = "http://localhost:8080")
+public interface BookSearchServiceClient {
+	@GetMapping("/api/books/search")
+	Page<BookSearchResponse> getSearchResult(
+		@RequestParam("keyword") String keyword,
+		@RequestParam(name = "search_type", required = false, defaultValue = "ALL") SearchType searchType,
+		@RequestParam(name = "sort_type", required = false, defaultValue = "ACCURACY") SortType sortType,
+		@PageableDefault Pageable pageable
+	);
+}
---
+++ src/main/java/shop/nuribooks/view/booksearch/service/impl/BookSearchServiceImpl.java
@@ -0,0 +1,24 @@
+package shop.nuribooks.view.booksearch.service.impl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import shop.nuribooks.view.booksearch.dto.BookSearchResponse;
+import shop.nuribooks.view.booksearch.enums.SearchType;
+import shop.nuribooks.view.booksearch.enums.SortType;
+import shop.nuribooks.view.booksearch.feign.BookSearchServiceClient;
+import shop.nuribooks.view.booksearch.service.BookSearchService;
+
+@Service
+@RequiredArgsConstructor
+public class BookSearchServiceImpl implements BookSearchService {
+	private final BookSearchServiceClient bookSearchServiceClient;
+
+	@Override
+	public Page<BookSearchResponse> getSearchResult(String keyword, SearchType searchType, SortType sortType,
+		Pageable pageable) {
+		return this.bookSearchServiceClient.getSearchResult(keyword, searchType, sortType, pageable);
+	}
+}
