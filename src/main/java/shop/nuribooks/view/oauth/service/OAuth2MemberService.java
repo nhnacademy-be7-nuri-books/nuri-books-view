b576807b1084b7ba2dc0511f0fc213e9357b15d7
Yongtaek Lee <itaekit@gmail.com>
2024-11-20 15:59:43 +0900

    feat: OAuth2 비밀번호 난수 생성 적용

--- src/main/java/shop/nuribooks/view/oauth/service/OAuth2MemberService.java
+++ src/main/java/shop/nuribooks/view/oauth/service/OAuth2MemberService.java
@@ -1,38 +1,35 @@
 package shop.nuribooks.view.oauth.service;
 
-import java.util.Objects;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import shop.nuribooks.view.member.member.feign.MemberServiceClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import shop.nuribooks.view.common.util.ExceptionUtil;
 import shop.nuribooks.view.member.member.dto.response.MemberRegisterResponse;
+import shop.nuribooks.view.member.member.feign.MemberServiceClient;
+import shop.nuribooks.view.oauth.common.utils.SecureRandomGenerator;
 import shop.nuribooks.view.oauth.dto.OAuth2RegisterRequest;
+
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class OAuth2MemberService {
-	private final MemberServiceClient memberServiceClient;
+    private final MemberServiceClient memberServiceClient;
 
-	@Value("${success.message-key}")
-	private String successMessageKey;
+    @Value("${success.message-key}")
+    private String successMessageKey;
 
-	public String signup(OAuth2RegisterRequest registerRequest) {
-		// TODO: 비밀번호 난수로 생성
-		registerRequest.setPassword("11111111");
-		registerRequest.setUsername(registerRequest.getUsername());
-		try {
-			MemberRegisterResponse response = memberServiceClient.registerUser(registerRequest.toRecord()).getBody();
-			return successMessageKey + Objects.requireNonNull(response).username();
-		} catch (FeignException ex) {
-			return ExceptionUtil.handleFeignException(ex);
-		}
-
-	}
+    public String signup(OAuth2RegisterRequest registerRequest) {
+        registerRequest.setPassword(SecureRandomGenerator.generateRandomString(15));
+        log.info("password : {}", registerRequest.getPassword());
+        try {
+            MemberRegisterResponse response = memberServiceClient.registerUser(registerRequest.toRecord()).getBody();
+            return successMessageKey + Objects.requireNonNull(response).username();
+        } catch (FeignException ex) {
+            return ExceptionUtil.handleFeignException(ex);
+        }
+    }
 }
