8b870be1243d652cc454836829b15b81f8192459
hxcva1 <hxcva1@gmail.com>
2024-11-21 13:13:16 +0900

    feat: 주소 수정 삭제 추가

--- src/main/java/shop/nuribooks/view/member/address/service/AddressClientService.java
+++ src/main/java/shop/nuribooks/view/member/address/service/AddressClientService.java
@@ -1,13 +1,20 @@
 package shop.nuribooks.view.member.address.service;
 
 import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+
 import shop.nuribooks.view.member.address.dto.request.AddressRegisterRequest;
+import shop.nuribooks.view.member.address.dto.request.AddressUpdateRequest;
 import shop.nuribooks.view.member.address.dto.response.AddressResponse;
 
 public interface AddressClientService {
 
-    void registerAddress(AddressRegisterRequest request);
+	ResponseEntity<Void> registerAddress(AddressRegisterRequest request);
 
-    ResponseEntity<List<AddressResponse>> getAddressList();
+	ResponseEntity<List<AddressResponse>> getAddressList();
+
+	ResponseEntity<Void> deleteAddress(Long addressId);
+
+	ResponseEntity<Void> updateAddress(Long addressId, AddressUpdateRequest request);
 }
