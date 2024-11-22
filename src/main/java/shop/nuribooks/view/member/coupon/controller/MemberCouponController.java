7f717ede95ccccb4bfb17dc939ab060c0d9687d5
jaaaayk <janghyun30@gmail.com>
2024-11-22 09:25:20 +0900

    Feature/membercoupon read (#120)
    * feature: member 조회용 feign cnrk
    
    * feat: view page 추가
    
    * feat: my-coupon view 추가
    
    * feat: my-coupon 완성

---
+++ src/main/java/shop/nuribooks/view/member/coupon/controller/MemberCouponController.java
@@ -0,0 +1,47 @@
+package shop.nuribooks.view.member.coupon.controller;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import shop.nuribooks.view.member.coupon.dto.MemberCouponResponse;
+import shop.nuribooks.view.member.coupon.service.MemberCouponService;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping
+public class MemberCouponController {
+	private final MemberCouponService memberCouponService;
+
+	@GetMapping("/my-coupon/available")
+	public String getAvailableCouponList(Model model,
+		Pageable pageable) {
+
+		Page<MemberCouponResponse> availableCoupons = memberCouponService.getAvailableCouponsByMemberId(pageable);
+
+		model.addAttribute("pages", availableCoupons);
+		model.addAttribute("paginationUrl", "/my-coupon/available");
+
+		return "member/my-coupons";
+	}
+
+	@GetMapping("/my-coupon/expired-or-used")
+	public String getExpiredOrUsedCouponList(Model model,
+		Pageable pageable) {
+
+		Page<MemberCouponResponse> expiredOrUsedCoupons = memberCouponService.getExpiredOrUsedCouponsByMemberId(
+			pageable);
+
+		model.addAttribute("pages", expiredOrUsedCoupons);
+		model.addAttribute("paginationUrl", "/my-coupon/expired-or-used");
+
+		return "member/my-coupons";
+	}
+
+}
