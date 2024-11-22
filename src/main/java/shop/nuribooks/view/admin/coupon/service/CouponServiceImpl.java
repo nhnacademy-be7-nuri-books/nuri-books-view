d0753f5f504c4198542e54e7e65ba296cd497a64
kkyongg <kkyongg2@gmail.com>
2024-11-22 17:40:21 +0900

    feat: 관리자 쿠폰페이지 유형별로 조회 적용

--- src/main/java/shop/nuribooks/view/admin/coupon/service/CouponServiceImpl.java
+++ src/main/java/shop/nuribooks/view/admin/coupon/service/CouponServiceImpl.java
@@ -7,17 +7,18 @@ import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import shop.nuribooks.view.admin.coupon.dto.CouponRequest;
 import shop.nuribooks.view.admin.coupon.dto.CouponResponse;
+import shop.nuribooks.view.admin.coupon.enums.CouponType;
 import shop.nuribooks.view.admin.coupon.feign.CouponServiceClient;
 import shop.nuribooks.view.common.dto.ResponseMessage;
 
 @Service
 @RequiredArgsConstructor
-public class CouponServiceImpl implements CouponService{
+public class CouponServiceImpl implements CouponService {
 	private final CouponServiceClient couponServiceClient;
 
 	@Override
-	public Page<CouponResponse> getCoupons(Pageable pageable) {
-		return this.couponServiceClient.getCoupons(pageable).getBody();
+	public Page<CouponResponse> getCoupons(CouponType type, Pageable pageable) {
+		return this.couponServiceClient.getCoupons(type, pageable).getBody();
 	}
 
 	@Override
