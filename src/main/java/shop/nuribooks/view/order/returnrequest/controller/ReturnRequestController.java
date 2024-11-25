5fccafa52394273c116e3c630d33880b7db6f38f
hxcva1 <hxcva1@gmail.com>
2024-11-25 17:03:08 +0900

    feat: 관리자 반품 요청 페이지 구현

--- src/main/java/shop/nuribooks/view/order/returnrequest/controller/ReturnRequestController.java
+++ src/main/java/shop/nuribooks/view/order/returnrequest/controller/ReturnRequestController.java
@@ -1,30 +1,48 @@
 package shop.nuribooks.view.order.returnrequest.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.nuribooks.view.order.returnrequest.dto.request.ReturnRequestRegisterRequest;
+import shop.nuribooks.view.order.returnrequest.dto.response.ReturnRequestResponse;
 import shop.nuribooks.view.order.returnrequest.service.ReturnRequestService;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/orders/return-requests")
 @Controller
 public class ReturnRequestController {
 
 	private final ReturnRequestService returnRequestService;
 
-	@PostMapping
+	@GetMapping("/admin/orders/return-requests")
+	public String getWrappingPapers(
+		@RequestParam(required = false, defaultValue = "0") int page,
+		@RequestParam(required = false, defaultValue = "5") int size,
+		Model model
+	) {
+		Pageable pageable = PageRequest.of(page, size);
+		Page<ReturnRequestResponse> pageResponse = returnRequestService.getReturnRequests(pageable);
+		model.addAttribute("pages", pageResponse);
+		return "admin/return-request/return-request";
+	}
+
+	@PostMapping("/orders/return-requests/{order-id}")
 	public ResponseEntity<Void> registerWrappingPaper(
-		@RequestParam("order-id") Long orderId,
-		@RequestParam("contents") String contents,
+		@PathVariable("order-id") Long orderId,
+		@RequestParam("contents") @NotBlank String contents,
 		@RequestParam("imageFile") MultipartFile imageFile
 	) {
 		String imageUrl = returnRequestService.uploadImage(imageFile);
--- src/main/java/shop/nuribooks/view/order/returnrequest/service/ReturnRequestServiceImpl.java
+++ src/main/java/shop/nuribooks/view/order/returnrequest/service/ReturnRequestServiceImpl.java
@@ -1,11 +1,14 @@
 package shop.nuribooks.view.order.returnrequest.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import shop.nuribooks.view.book.feign.BookServiceClient;
 import shop.nuribooks.view.order.returnrequest.dto.request.ReturnRequestRegisterRequest;
+import shop.nuribooks.view.order.returnrequest.dto.response.ReturnRequestResponse;
 import shop.nuribooks.view.order.returnrequest.feign.ReturnRequestServiceClient;
 
 @RequiredArgsConstructor
@@ -17,7 +20,12 @@ public class ReturnRequestServiceImpl implements ReturnRequestService {
 
 	@Override
 	public void registerReturnRequest(Long orderId, ReturnRequestRegisterRequest returnRequestRegisterRequest) {
+		returnRequestServiceClient.registerReturnRequest(orderId, returnRequestRegisterRequest);
+	}
 
+	@Override
+	public Page<ReturnRequestResponse> getReturnRequests(Pageable pageable) {
+		return returnRequestServiceClient.getReturnRequests(pageable).getBody();
 	}
 
 	@Override
