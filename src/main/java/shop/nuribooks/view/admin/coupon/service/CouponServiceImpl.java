59fc457ce3739f57383459a1c02ba334329934b2
jaaaayk <janghyun30@gmail.com>
2024-11-26 09:21:18 +0900

    Feature/admin category coupon (#153)
    * merge: before
    
    * feat: 카테고리 쿠폰 추가
    
    * fix:  필드 이름 수정

--- src/main/java/shop/nuribooks/view/admin/coupon/service/CouponServiceImpl.java
+++ src/main/java/shop/nuribooks/view/admin/coupon/service/CouponServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import shop.nuribooks.view.admin.category.feign.AdminCategoryClient;
 import shop.nuribooks.view.admin.coupon.dto.BookCouponRequest;
+import shop.nuribooks.view.admin.coupon.dto.CategoryCouponRequest;
 import shop.nuribooks.view.admin.coupon.dto.CouponRequest;
 import shop.nuribooks.view.admin.coupon.dto.CouponResponse;
 import shop.nuribooks.view.admin.coupon.enums.CouponType;
@@ -16,6 +18,7 @@ import shop.nuribooks.view.common.dto.ResponseMessage;
 @RequiredArgsConstructor
 public class CouponServiceImpl implements CouponService {
 	private final CouponServiceClient couponServiceClient;
+	private final AdminCategoryClient adminCategoryClient;
 
 	@Override
 	public Page<CouponResponse> getCoupons(CouponType type, Pageable pageable) {
@@ -41,4 +44,10 @@ public class CouponServiceImpl implements CouponService {
 	public ResponseMessage expireCoupon(Long id) {
 		return this.couponServiceClient.expireCoupon(id).getBody();
 	}
+
+	@Override
+	public ResponseMessage registerCategoryCoupon(CategoryCouponRequest couponRequest) {
+		return this.couponServiceClient.registerCategoryCoupon(couponRequest).getBody();
+	}
+
 }
