5e56d2bed51091b39053cc91c9891f3a5664f277
kkyongg <kkyongg2@gmail.com>
2024-11-26 19:51:15 +0900

    feat: 관리자 배치 작업 생일쿠폰 적용

--- src/main/java/shop/nuribooks/view/admin/batch/AdminBatchController.java
+++ src/main/java/shop/nuribooks/view/admin/batch/AdminBatchController.java
@@ -1,40 +1,48 @@
 package shop.nuribooks.view.admin.batch;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/admin/batch")
 public class AdminBatchController {
 
-    private final AdminBatchClient adminBatchClient;
+	private final AdminBatchClient adminBatchClient;
 
-    @GetMapping
-    public String showBatchList() {
-        return "admin/batch/batch-list";
-    }
+	@GetMapping
+	public String showBatchList() {
+		return "admin/batch/batch-list";
+	}
 
-    @GetMapping("/inactive")
-    public String inactiveMembersJob() {
-        adminBatchClient.inactiveMembersJob();
+	@GetMapping("/inactive")
+	public String inactiveMembersJob() {
+		adminBatchClient.inactiveMembersJob();
 
-        return "admin/batch/batch-list";
-    }
+		return "admin/batch/batch-list";
+	}
 
-    @GetMapping("/withdraw")
-    public String softDeleteMembersJob() {
-        adminBatchClient.softDeleteMembersJob();
+	@GetMapping("/withdraw")
+	public String softDeleteMembersJob() {
+		adminBatchClient.softDeleteMembersJob();
 
-        return "admin/batch/batch-list";
-    }
+		return "admin/batch/batch-list";
+	}
 
-    @GetMapping("/grade")
-    public String membersGradeUpdateJob() {
-        adminBatchClient.membersGradeUpdateJob();
+	@GetMapping("/grade")
+	public String membersGradeUpdateJob() {
+		adminBatchClient.membersGradeUpdateJob();
 
-        return "admin/batch/batch-list";
-    }
+		return "admin/batch/batch-list";
+	}
+
+	@GetMapping("/birthday")
+	public String issueBirthdayCouponJob() {
+		adminBatchClient.issueBirthdayCouponJob();
+
+		return "admin/batch/batch-list";
+	}
 }
