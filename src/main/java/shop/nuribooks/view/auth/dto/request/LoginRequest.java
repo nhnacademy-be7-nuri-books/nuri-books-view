7c62928d943f26b90de45fc2e9368d8e2828e82c
Jprotection <bigho98@gmail.com>
2024-11-28 18:03:37 +0900

    feat: 회원가입, 로그인 화면 실시간 유효성 검증 (#178)

--- src/main/java/shop/nuribooks/view/auth/dto/request/LoginRequest.java
+++ src/main/java/shop/nuribooks/view/auth/dto/request/LoginRequest.java
@@ -1,7 +1,7 @@
 package shop.nuribooks.view.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 
 /**
  * 로드인 요청 데이터 객체
@@ -11,12 +11,14 @@ import jakarta.validation.constraints.Size;
  */
 public record LoginRequest(
 
-	@NotBlank(message = "사용자 이름은 필수 입력입니다.")
-	@Size(min = 8, max = 20, message = "사용자 아이디는 8자 이상 20자 이하로 입력해야 합니다.")
-	String username,
+        @NotBlank(message = "아이디는 반드시 입력해야 합니다.")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])[a-z0-9]{8,20}$",
+                message = "아이디는 8자 이상 20자 이하, 영어 소문자와 숫자가 각각 1개 이상 포함되어야 합니다.")
+        String username,
 
-	@NotBlank(message = "비밀번호는 필수 입력입니다.")
-	@Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해야 합니다.")
-	String password
+        @NotBlank(message = "비밀번호는 반드시 입력해야 합니다.")
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$",
+                message = "비밀번호는 8자 이상 20자 이하, 영문자, 숫자, 특수문자가 각각 1개 이상 포함되어야 합니다.")
+        String password
 ) {
 }
